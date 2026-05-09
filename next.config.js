7c6453d7f7a7c8582723737c192b78cdb443842f
kavin : ) <kavinsmac@gmail.com>
2026-05-09 20:31:52 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      "graph.facebook.com",
+      "i.ytimg.com",
+      "yt3.ggpht.com",
+      "media.licdn.com",
+      "cdninstagram.com",
+      "scontent.cdninstagram.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;
